452e2f0272402775bafd2a80fdb815697eb6b5e7
Naghmeh <naghmeh.asad@gmail.com>
2022-04-15 21:44:29 +0430

    connect css js links

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -16,19 +16,19 @@
 	<script src="themes/js/less.js" type="text/javascript"></script> -->
 	
 <!-- Bootstrap style --> 
-    <link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
-    <link href="themes/css/base.css" rel="stylesheet" media="screen"/>
-<!-- Bootstrap style responsive -->	
-	<link href="themes/css/bootstrap-responsive.min.css" rel="stylesheet"/>
-	<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
-<!-- Google-code-prettify -->	
-	<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+<link id="callCss" rel="stylesheet" href="{{asset('themes/bootshop/bootstrap.min.css')}}" media="screen"/>
+<link href="{{asset('themes/css/base.css')}}" rel="stylesheet" media="screen"/>
+<!-- Bootstrap style responsive --> 
+<link href="{{asset('themes/css/bootstrap-responsive.min.css')}}" rel="stylesheet"/>
+<link href="{{asset('themes/css/font-awesome.css')}}" rel="stylesheet" type="text/css">
+<!-- Google-code-prettify -->   
+<link href="{{asset('themes/js/google-code-prettify/prettify.css')}}" rel="stylesheet"/>
 <!-- fav and touch icons -->
-    <link rel="shortcut icon" href="themes/images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="{{asset('themes/images/ico/favicon.ico')}}">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('themes/images/ico/apple-touch-icon-144-precomposed.png')}}">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('themes/images/ico/apple-touch-icon-114-precomposed.png')}}">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('themes/images/ico/apple-touch-icon-72-precomposed.png')}}">
+<link rel="apple-touch-icon-precomposed" href="{{asset('themes/images/ico/apple-touch-icon-57-precomposed.png')}}">
 	<style type="text/css" id="enject"></style>
   </head>
 <body>
@@ -532,12 +532,11 @@
 	</div><!-- Container End -->
 	</div>
 <!-- Placed at the end of the document so the pages load faster ============================================= -->
-	<script src="themes/js/jquery.js" type="text/javascript"></script>
-	<script src="themes/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="themes/js/google-code-prettify/prettify.js"></script>
-	
-	<script src="themes/js/bootshop.js"></script>
-    <script src="themes/js/jquery.lightbox-0.5.js"></script>
+<script src="{{asset('themes/js/jquery.js')}}" type="text/javascript"></script>
+<script src="{{asset('themes/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('themes/js/google-code-prettify/prettify.js')}}"></script>
+<script src="{{asset('themes/js/bootshop.js')}}"></script>
+<script src="{{asset('themes/js/jquery.lightbox-0.5.js')}}"></script>
 	
 	<!-- Themes switcher section ============================================================================================= -->
 <div id="secectionBox">
