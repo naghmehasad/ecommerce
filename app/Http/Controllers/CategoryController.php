9c9d334c0e982cbc7a589696da8ad06b59867f6b
Naghmeh <naghmeh.asad@gmail.com>
2022-04-22 00:45:12 +0430

    Create category and subcategory by one form

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+
 
 class CategoryController extends Controller
 {
-    //
+    public function create(){
+        $categories = Category::whereNull('category_id')->get();
+        return view('admin.category.add',compact('categories'));
+    }
+
+    public function store(Request $request) {
+        $data = array(
+            'name' => $request->name,
+            'category_id' => $request->category_id,
+        );
+        $create = Category::create($data);
+        return redirect()->route('category.create');
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BaseController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,4 +34,12 @@ Route::post('/admin/login', [AdminController::class,'makeLogin'])->name('admin.m
 Route::group(['middleware' =>'auth'],function(){ 
 	Route::get('/admin/dashboard', [AdminController::class,'dashboard'])->name('admin.dashboard');
 	Route::get('/admin/logout', [AdminController::class,'logout'])->name('admin.logout');
+
+
+	/* CategoryController routes */
+	Route::get('/category/add', [CategoryController::class,'create'])->name('category.create');
+	Route::post('/category/add', [CategoryController::class,'store'])->name('category.store');
+
+	
+
 });
